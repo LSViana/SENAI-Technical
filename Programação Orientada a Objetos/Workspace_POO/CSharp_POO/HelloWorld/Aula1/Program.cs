32ee682f9d4ef1dbdb2909ef1ae0c84dc373061f
LSViana <lv201122@gmail.com>
2017-07-27 13:17:46 -0300

    First commit about SENAI_Technical repository
    It keeps all content made during SENAi Technical Course between 2nd and
    4th semesters

---
+++ Programação Orientada a Objetos/Workspace_POO/CSharp_POO/HelloWorld/Aula1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //
+            Console.ReadKey();
+        }
+        static void Variables()
+        {
+            String Name = "Jorge Rabello";
+            int Age = 29;
+            Boolean Married = true;
+            //
+            Console.WriteLine("Name: {0}\nAge: {1}\nMarried: {2}");
+        }
+        static void HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Programação Orientada a Objetos/Workspace_POO/Java_POO/HelloWorld/src/Class1/Program.java
@@ -0,0 +1,22 @@
+package Class1;
+
+public class Program
+{
+	public static void main(String[] args)
+	{
+		
+	}
+	static void Variables()
+	{
+		String nome = "Jorge Rabello";
+		int idade = 29;
+		boolean casado = true;
+		//
+		System.out.printf("Nome: %s\nIdade: %d\nCasado: %b",
+				nome, idade, casado);
+	}
+	static void HelloWorld()
+	{
+		System.out.println("Hello World!");
+	}
+}
