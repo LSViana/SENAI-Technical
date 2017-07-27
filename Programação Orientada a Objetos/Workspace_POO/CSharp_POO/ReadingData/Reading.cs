53b3de137b953cd27b6c1c2c870e0d656ff9e014
LSViana <lv201122@gmail.com>
2017-07-27 14:20:09 -0300

    Changing and creating a new commit from VSCode

--- Programação Orientada a Objetos/Workspace_POO/CSharp_POO/ReadingData/Reading.cs
+++ Programação Orientada a Objetos/Workspace_POO/CSharp_POO/ReadingData/Reading.cs
@@ -1,7 +1,13 @@
 using System;
+using System.Windows.Forms;
 
 public  class Reading {
 	public static void Main() {
+		Form a = new Form();
+		a.ShowDialog();
+	}
+
+	public static void UsingConsole() {
 		Console.Write("What is your name? ");
 		String userName = Console.ReadLine();
 		Console.WriteLine("Welcome, {0}!", userName);
--- Programação Orientada a Objetos/Workspace_POO/Java_POO/ReadingData/Reading.java
+++ Programação Orientada a Objetos/Workspace_POO/Java_POO/ReadingData/Reading.java
@@ -1,8 +1,19 @@
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Reading {
 	private static Scanner scanner;
+
 	public static void main(String[] args) {
+		usingJOptionPane();
+	}
+
+	public static void usingJOptionPane() {
+		String name = JOptionPane.showInputDialog(null, "What is your name?");
+		JOptionPane.showMessageDialog(null, "Welcome " + name + "!");
+	}
+
+	public static void usingScanner() {
 		scanner = new Scanner(System.in);
 		//
 		System.out.print("What is your name? ");
