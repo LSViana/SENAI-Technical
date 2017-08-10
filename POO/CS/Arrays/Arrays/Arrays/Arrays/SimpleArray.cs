033d973995c6b08d74849490f886b6a7f468e73f
LSViana <lv201122@gmail.com>
2017-08-10 14:49:20 -0300

    Saving changes to a lightweight project

---
+++ POO/CS/Arrays/Arrays/Arrays/Arrays/SimpleArray.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class SimpleArray
+    {
+        public SimpleArray()
+        {
+            // Declaring a new array
+            // <type>[] <var_name> = new <type>[<capacity>];
+
+            // First way to declare an array
+            String[] countries1 = new String[4];
+            countries1[0] = "Brasil";
+            countries1[1] = "Rússia";
+            countries1[2] = "Índia";
+            countries1[3] = "China";
+
+            // Second way to declare an array
+            String[] countries2 = { "Brasil", "Rússia", "Índia", "China" };
+
+            // Using foreach structure to print an array
+            foreach(String country in countries1)
+                Console.WriteLine(country + " ");
+            Console.WriteLine();
+
+            // Looking for a value inside an Array, but it is able to return only 0 (zero) or 1 (one), because it is made to work on trees, where each branch has two other
+            int russiaIndex = Array.BinarySearch(countries1, "Rússia");
+            Console.WriteLine("'Rússia' found at: " + russiaIndex);
+
+            // Ordering an Array using Arrays.sort class method
+            Array.Sort(countries1, 0, countries1.Length);
+
+            // Printing an ordered array
+            foreach(String country in countries1)
+                Console.WriteLine(country + " ");
+            Console.WriteLine();
+        }
+    }
+}
