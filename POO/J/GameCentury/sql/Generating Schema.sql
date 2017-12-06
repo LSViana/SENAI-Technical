8d8675603147d54696123c4c3ff81602d074b1ec
LSViana <lv201122@gmail.com>
2017-12-06 17:00:26 -0200

    Updating Terminal 132

---
+++ POO/J/GameCentury/sql/Generating Schema.sql
@@ -0,0 +1,19 @@
+DROP SCHEMA `gamecentury`;
+
+CREATE SCHEMA `gamecentury` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+
+USE `gamecentury`;
+
+CREATE TABLE `user` (
+	`id` int primary key auto_increment,
+	`name` varchar(128) not null,
+	`username` varchar(32) not null,
+	`password` varchar(32) not null
+);
+
+INSERT INTO `user` VALUES(0, 'Lucas', 'lsviana', 'lv201122');
+INSERT INTO `user` VALUES(0, 'Rafael', 'thayto', 'thayto777');
+
+SELECT * FROM `user`;
+
+SELECT * FROM `user` WHERE `username` = 'lsviana';
