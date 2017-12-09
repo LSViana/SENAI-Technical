41a3f6ca5415cfcbc0370b484c18122b42b9b0ef
LSViana <lv201122@gmail.com>
2017-12-08 23:10:22 -0200

    Updating Torres' Word animations

--- IWB/CSS/Torres' Word/js/main.js
+++ IWB/CSS/Torres' Word/js/main.js
@@ -1,30 +1,77 @@
 var inputs = document.getElementsByTagName("input");
 var firstPhrase = document.getElementById("first-phrase");
+var secondPhrase = document.getElementById("second-phrase");
 var mainWord = document.getElementById("main-word");
-for (var index = 0; index < inputs.length; index++) {
-    var element = inputs[index];
-    if (index == inputs.length - 1) {
-        element.addEventListener("input", function(a) {
-            var word = inputs[0].value + inputs[1].value + inputs[2].value + inputs[3].value;
-            if(word == "like") {
-                firstPhrase.classList.add("quick-exit-right");
-                Array.prototype.forEach.call(inputs, function(value, index, listObj) {
-                    value.classList.add("remove-borders");
-                });
+// Dynamic
+function initializeInput(input) {
+    // Typing
+    var letterPosition = Number(input.id.substr(6));
+    input.addEventListener("keydown", function(event) {
+        if(event.code == "Backspace") {
+            if(inputs.length != 1) {
+                this.remove();
+                var previous = document.getElementById("letter" + (letterPosition - 1));
+                previous.focus();
+                window.getSelection().removeAllRanges();
             }
-        });
-    }
-    element.addEventListener("input", function (a) {
-        var baseId = this.id.substr(0, this.id.length - 1);
-        var nextId = baseId + (Number(this.id.substring(this.id.length - 1)) + 1);
-        var previousId = baseId + (Number(this.id.substring(this.id.length - 1)) - 1);
-        var nextElement;
-        if (a.data == null) {
-            nextElement = document.getElementById(previousId);
-        } else {
-            nextElement = document.getElementById(nextId);
+            else {
+                this.value = null;
+                return;
+            }
         }
-        if(nextElement != undefined)
-            nextElement.focus();
+        else if(this.value.length > 0) {
+            if(inputs.length >= 10 || event.key.length > 1) {
+                return;
+            }
+            var freshInput = document.createElement("input");
+            mainWord.appendChild(freshInput);
+            freshInput.setAttribute("maxlength", "1");
+            freshInput.setAttribute("id", "letter" + (letterPosition + 1));
+                freshInput.value = event.key;
+            initializeInput(freshInput);
+            freshInput.focus();
+        }
+        inputs = document.getElementsByTagName("input");
+        var word = "";
+        Array.prototype.forEach.call(inputs, function(a, b, c) {
+            word += a.value;
+        });
+        word = word.toLowerCase().trim();
+        if(word == "love") {
+            console.log(1);
+            $("#main-box img").css({
+                "display": "unset"
+            })
+            .animate({
+                "opacity": "1"
+            });
+            $("#main-box *").removeClass("make-pinker");
+            $("#main-box *").addClass("make-golder");
+        }
+        else if(word == "like") {
+            console.log(2);
+            $("#main-box p").slideUp(1000);
+            $("#upper-phrase, #lower-phrase").animate({
+                "opacity": "1"
+            });
+        }
+        else {
+            console.log(3);
+            $("#main-box img").css({
+                "display": "none"
+            })
+            .animate({
+                "opacity": "0"
+            });
+            $("#upper-phrase, #lower-phrase").animate({
+                "opacity": "0"
+            });
+            $("#main-box *").removeClass("make-golder");
+            $("#main-box *").addClass("make-pinker");
+            $("#main-box *").show();
+        }
     });
 }
+Array.prototype.forEach.call(inputs, function(a, b, c) {
+    initializeInput(a);
+});
