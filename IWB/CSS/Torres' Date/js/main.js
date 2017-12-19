602fdfc067f77ad3139d4c12da1ef8606a751249
LSViana <lv201122@gmail.com>
2017-12-19 08:00:54 -0200

    Changing Date analysis way to become universal and ensure working with ISO standard

--- IWB/CSS/Torres' Date/js/main.js
+++ IWB/CSS/Torres' Date/js/main.js
@@ -16,26 +16,29 @@ var intId = setInterval(function () {
     var spentDays = new Date((endDate - now)).getTime() / (1000 * 60 * 60 * 24);
     var perc = Math.floor((days - spentDays) / days * 100);
     // Loading some themes
-    var textDate = now.toLocaleDateString();
-    if (textDate.indexOf("2017") != -1) {
-        if (textDate.indexOf("12") == 0) {
-            if (textDate.indexOf("25") == 3) {
+    var textDate = now.toISOString().substr(0, 10);
+    var year = Number(textDate.substr(0, 4));
+    var month = Number(textDate.substr(5, 2));
+    var day = Number(textDate.substr(8, 2));
+    if (year == 2017) {
+        if (month == 12) {
+            if (day == 25) {
                 // Christmas Theme
                 timeLine.classList.add("christmas-header");
                 tasks.classList.add("christas-main");
-            } else if (textDate.indexOf("18") == 3 || textDate.indexOf("19") == 3) {
+            } else if (day >= 18 && day <= 19) {
                 // Miami Theme
                 timeLine.classList.add("miami-header");
                 tasks.classList.add("miami-main");
-            } else if (textDate.indexOf("20") == 3) {
+            } else if (day == 20) {
                 // Disney Theme
                 timeLine.classList.add("disney-header");
                 tasks.classList.add("disney-main");
             }
         }
-    } else if (textDate.indexOf("2018") != -1) {
-        if (textDate.indexOf("1") == 0) {
-            if (textDate.indexOf("1") == 2) {
+    } else if (year = 2018) {
+        if (month == 1) {
+            if (day == 1) {
                 // New Year
                 timeLine.classList.add("new-year-header");
                 tasks.classList.add("new-year-main");
