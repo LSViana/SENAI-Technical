deba1d9980fbe1cb3974b602044aa7ff0566cbe9
LSViana <lv201122@gmail.com>
2018-03-07 20:11:29 -0300

    Start developing ShopPlace at Terminal 3

--- PWB/ShopPlace/sql/Generate.sql
+++ PWB/ShopPlace/sql/Generate.sql
@@ -3,12 +3,20 @@ CREATE DATABASE shopplace DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 USE shopplace;
 
 CREATE TABLE user (
-	id bigint auto_increment primary key,
+	id bigint unsigned auto_increment primary key,
     name varchar(64) not null,
-    username varchar(32) not null,
-    password varchar(512) not null
+    email varchar(32) not null,
+    password varchar(512) not null,
+    dateofbirth date not null
 );
 
-TRUNCATE TABLE user;
+CREATE TABLE product (
+	id bigint unsigned auto_increment primary key,
+	name varchar(64) not null,
+	price decimal(10, 2) not null,
+	registerdate datetime not null
+);
 
 SELECT * FROM user;
+
+SELECT * FROM product;
